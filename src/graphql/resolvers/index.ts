96a2f03a8725edad2a89d6983a577e63ff682b2b
ayankovsky <p44gr10@gmail.com>
2020-10-16 22:22:07 +0200

    Made to parameter optional

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,7 +1,12 @@
+import { DateTime } from 'luxon'
 import { traceService, unitErrorService, unitService, insightService, settingsService } from '../../service'
 import { cloudWatchLogsService } from '../../service/aws/cloud-watch-logs.service'
 import { SlackService } from '../../service/notification/slack.service'
 import { Settings } from '../../entity/pg'
+
+const numberOr = (str: string | undefined, defaultValue: number): number => (
+  str ? Number(str) : defaultValue
+)
 
 export default {
   Query: {
@@ -16,15 +21,15 @@ export default {
       )
     ),
     getTrace: (obj: any, args: any) => traceService.getTrace(args.id),
-    getErrors: (obj: any, args: any) => unitErrorService.getErrors(Number(args.from), Number(args.to), args.offset),
+    getErrors: (obj: any, args: any) => unitErrorService.getErrors(Number(args.from), numberOr(args.to, DateTime.utc().startOf('hour').toMillis()), args.offset),
     getError: (obj: any, args: any) => unitErrorService.getError(Number(args.id)),
-    getErrorStats: (obj: any, args: any) => unitErrorService.getErrorStats(Number(args.id), Number(args.from), Number(args.to)),
-    getUnits: (obj: any, args: any) => unitService.getUnits(args.from, args.to, args.search, args.offset, args.orderBy, args.orderDirection),
-    getUnit: (obj: any, args: any) => unitService.getUnit(args.unitName, Number(args.from), Number(args.to)),
-    getInsights: (obj: any, args: any) => insightService.getInsights(Number(args.from), Number(args.to)),
-    getTotalStats: (obj: any, args: any) => traceService.getTotalStats(Number(args.from), Number(args.to)),
-    getNewErrors: (obj: any, args: any) => unitErrorService.getNewErrors(Number(args.from), Number(args.to)),
-    getTopInvokedUnits: (obj: any, args: any) => unitService.getTopInvokedUnits(Number(args.from), Number(args.to)),
+    getErrorStats: (obj: any, args: any) => unitErrorService.getErrorStats(Number(args.id), Number(args.from), numberOr(args.to, DateTime.utc().startOf('hour').toMillis())),
+    getUnits: (obj: any, args: any) => unitService.getUnits(args.from, numberOr(args.to, DateTime.utc().startOf('hour').toMillis()), args.search, args.offset, args.orderBy, args.orderDirection),
+    getUnit: (obj: any, args: any) => unitService.getUnit(args.unitName, Number(args.from), numberOr(args.to, DateTime.utc().startOf('hour').toMillis())),
+    getInsights: (obj: any, args: any) => insightService.getInsights(Number(args.from), numberOr(args.to, DateTime.utc().startOf('hour').toMillis())),
+    getTotalStats: (obj: any, args: any) => traceService.getTotalStats(Number(args.from), numberOr(args.to, DateTime.utc().startOf('hour').toMillis())),
+    getNewErrors: (obj: any, args: any) => unitErrorService.getNewErrors(Number(args.from), numberOr(args.to, DateTime.utc().startOf('hour').toMillis())),
+    getTopInvokedUnits: (obj: any, args: any) => unitService.getTopInvokedUnits(Number(args.from), numberOr(args.to, DateTime.utc().startOf('hour').toMillis())),
     getSettings: () => settingsService.getGraphqlSettings(),
   },
   Mutation: {
--- src/graphql/schema/query.ts
+++ src/graphql/schema/query.ts
@@ -2,17 +2,17 @@ import { gql } from 'apollo-server-express'
 
 export const Query = gql`
   type Query {
-    getErrors(from: String!, to: String!, offset: Int): GetUnitErrorsResponse!
-    getErrorStats(id: String!, from: String!, to: String!): [UnitErrorDetailsGraphStats!]!
+    getErrors(from: String!, to: String, offset: Int): GetUnitErrorsResponse!
+    getErrorStats(id: String!, from: String!, to: String): [UnitErrorDetailsGraphStats!]!
     getError(id: String!): UnitError
     getTraces(search: String, offset: Int, unitName: String, unitErrorId: String, limit: Int, statuses: [String!]): GetTracesResponse!
     getTrace(id: String!): Trace
-    getUnits(from: String!, to: String!, search: String, offset: Int, orderBy: String, orderDirection: String): GetUnitsResponse!
-    getUnit(unitName: String!, from: String!, to: String!): UnitDetails
-    getInsights(from: String!, to: String!): [Insight!]!
-    getTotalStats(from: String!, to: String!): TotalStats!
-    getNewErrors(from: String!, to: String!): [UnitError!]!
-    getTopInvokedUnits(from: String!, to: String!): [TopInvokedUnit!]!
+    getUnits(from: String!, to: String, search: String, offset: Int, orderBy: String, orderDirection: String): GetUnitsResponse!
+    getUnit(unitName: String!, from: String!, to: String): UnitDetails
+    getInsights(from: String!, to: String): [Insight!]!
+    getTotalStats(from: String!, to: String): TotalStats!
+    getNewErrors(from: String!, to: String): [UnitError!]!
+    getTopInvokedUnits(from: String!, to: String): [TopInvokedUnit!]!
     getSettings: Settings!
   }
 `
